55756dd931a514a7ef5cfc1e0dce1936800da707
Rainer 'rei' Schuth <devel@reixd.net>
2019-10-10 15:40:36 +0200

    Add usefull wrapper scripts for DebOps

---
+++ scripts/install_ansible_debops_controller.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# https://docs.debops.org/en/master/user-guide/install.html
+#
+# This scripts installs DebOps and its dependencies on a (fresh) system.
+# It creates a dedicated DebOps user (e.g. ansible) who should then execute all DebOps-playbooks.
+# This script only installs DebOps globally , it does not initiliaze any DebOps-project.
+
+set -o nounset -o pipefail -o errexit
+
+# The username used by debops
+DEBOPS_ADMIN_USER="${DEBOPS_ADMIN_USER:-ansible}"
+
+echo "Install dependencies"
+apt-get -q update
+DEBIAN_FRONTEND=noninteractive apt-get \
+  --no-install-recommends -yq install \
+  iproute2 \
+  levee \
+  openssh-client \
+  python3-apt \
+  python3-distro \
+  python3-dnspython \
+  python3-future \
+  python3-ldap \
+  python3-openssl \
+  python3-pip \
+  python3-wheel \
+  python3-setuptools \
+  procps \
+  sudo \
+  tree \
+  git \
+  gpgv2 \
+  uuid-runtime \
+  sshpass \
+  encfs \
+  dirmngr \
+  rsync \
+  pass
+
+echo "Install ansible and debops"
+pip3 install \
+ debops[ansible]
+
+echo "Cleaning up cache directories..."
+apt clean
+rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/*.deb /root/.cache/*
+
+echo "Add '${DEBOPS_ADMIN_USER}' admin user"
+groupadd --system admins
+echo "%admins ALL = (ALL:ALL) NOPASSWD: SETENV: ALL" > /etc/sudoers.d/admins
+chmod 0440 /etc/sudoers.d/admins
+useradd --user-group --create-home --shell /bin/bash \
+       --home-dir /home/${DEBOPS_ADMIN_USER} --groups admins \
+       --comment '${DEBOPS_ADMIN_USER} Admin User' ${DEBOPS_ADMIN_USER}
---
+++ scripts/provisioning_ldap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+################################################################################
+################################################################################
+#
+# This script calls the needed DebOps plays/roles in order to provision a new cloud VM,
+# then prepares the VM to be ansible managed and runs additional Ansible Playbooks in the correct order.
+#
+################################################################################
+
+set -o nounset -o pipefail -o errexit
+
+# Check if we are in the correct root folder of the repo, and change upwards if needed
+# otherwise debops won't work
+if [[ ! -f .debops.cfg && -f ../.debops.cfg ]]; then
+    echo "Running $0 from root of repo."
+    pushd .. > /dev/null
+fi
+
+# Run the bootstrap DebOps playbook to make the VM a DebOps/Ansible managed host.
+debops --become $@ bootstrap-ldap || debops --become --user root $@ bootstrap-ldap
+
+# Execute all DebOps plays (site.yml) to install everything.
+debops $@
+
+# Run the PKI role a second time (the first is executed from the site.yml DebOps playbook)
+#   The second run is necessary for DebOps to download/request the Let's Encrypt ACME TLS certificates
+#   Somehow during the first run, only the internal CA TLS Certificates are installed, but not the ACME certificates.
+#   Running role::pki helps to get the ACME certs.
+debops -t role::pki $@
