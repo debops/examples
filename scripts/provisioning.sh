2ae7aa23263b4432e83890b636e039d59e1cefd4
Rainer 'rei' Schuth <devel@reixd.net>
2019-10-12 10:52:23 +0200

    Fix typo

--- scripts/provisioning.sh
+++ scripts/provisioning.sh
@@ -24,7 +24,7 @@ if [[ ! -f .debops.cfg && -f ../.debops.cfg ]]; then
 fi
 
 # Run the bootstrap DebOps playbook to make the VM a DebOps/Ansible managed host.
-debops $@ bootstrap-ldap || debops --user root $@ bootstrap-ldap
+debops $@ bootstrap || debops --user root $@ bootstrap
 
 # Execute all DebOps plays (site.yml) to install everything.
 debops $@
